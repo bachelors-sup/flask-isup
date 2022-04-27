c08bee2046b3ccf47c3f25e45785ba6f47e5e245
Julien Palard <julien@palard.fr>
2022-04-27 09:33:33 +0200

    Listing domains.

--- app.py
+++ app.py
@@ -3,20 +3,23 @@ from flask import g, Flask
 
 app = Flask(__name__)
 
-DATABASE = 'db.sqlite3'
+DATABASE = "db.sqlite3"
+
 
 def get_db():
-    db = getattr(g, '_database', None)
+    db = getattr(g, "_database", None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
     db.row_factory = sqlite3.Row
     return db
 
+
 @app.teardown_appcontext
 def close_connection(exception):
-    db = getattr(g, '_database', None)
+    db = getattr(g, "_database", None)
     if db is not None:
         db.close()
+
 
 def query_db(query, args=(), one=False):
     cur = get_db().execute(query, args)
@@ -24,7 +27,25 @@ def query_db(query, args=(), one=False):
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
-@app.route('/')
+
+def get_domains():
+    return query_db("""SELECT * FROM domains""")
+
+
+@app.route("/")
 def index():
-    cur = get_db().cursor()
-    ...
+    return {"/domains/": "Domain names to monitor."}
+
+
+@app.route("/domains/")
+def domains():
+    return {
+        "domains": [
+            {
+                "name": domain['name'],
+                "is_up": bool(domain['is_up']),
+                "last_check": domain['last_check'],
+            }
+            for domain in get_domains()
+        ]
+    }
